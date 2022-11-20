cdb7ad9153e125ae53be2fa7d08f0419cd535ae7
Vladimir <87763796+moiseevv@users.noreply.github.com>
2022-11-21 06:57:00 +0700

    main

---
+++ spark_word_count.py
@@ -0,0 +1,5 @@
+file = spark.textFile("hdfs://...")
+counts = file.flatMap( lambda line: line.split(" "))
+	.map(lambda word: (word, 1))
+	.reduceByKey(lambda a, b: a + b)
+counts.saveAsTextFile("hdfs://..")
